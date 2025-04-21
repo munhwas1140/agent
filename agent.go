fa3ed77758a9290857a3d670c50951be68391df7
HyungJunNoh <munhwas1140@gmail.com>
2025-04-21 13:13:04 +0900

    refactor: remove comment

--- agent.go
+++ agent.go
@@ -92,14 +92,6 @@ func (a *Agent) executeTool(id string, name string, input json.RawMessage) anthr
 	return anthropic.NewToolResultBlock(id, response, false)
 }
 
-// In this environment you have access to a set of tools you can use to answer the user's question.
-// {{ FORMATTING INSTRUCTIONS }}
-// String and scalar parameters should be specified as is, while lists and objects should use JSON format. Note that spaces for string values are not stripped. The output is not expected to be valid XML and is parsed with regular expressions.
-// Here are the functions available in JSONSchema format:
-// {{ TOOL DEFINITIONS IN JSON SCHEMA }}
-// {{ USER SYSTEM PROMPT }}
-// {{ TOOL CONFIGURATION }}
-
 func (a *Agent) runInterface(ctx context.Context, conversation []anthropic.MessageParam) (*anthropic.Message, error) {
 	anthropicTools := []anthropic.ToolUnionParam{}
 
